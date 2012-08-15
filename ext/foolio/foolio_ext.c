9f4f2338fb1b7f6022d9321625368a7401df9769
mzp <mzpppp@gmail.com>
2012-08-16 00:44:25 +0900

    add uv_tcp_xxx

--- ext/foolio/foolio_ext.c
+++ ext/foolio/foolio_ext.c
@@ -106,6 +106,142 @@ static VALUE run_once(VALUE self, VALUE loop) {
   return INT2NUM(uv_run_once(loop_p));
 }
 
+static VALUE ip4_addr(VALUE ip, VALUE port) {
+  struct sockaddr_in* p = malloc(sizeof(struct sockaddr_in));
+  *p = uv_ip4_addr((const char*)StringValueCStr(ip), NUM2INT(port));
+  return Wrap(p, free);
+}
+
+static VALUE ip6_addr(VALUE ip, VALUE port) {
+  struct sockaddr_in6* p = malloc(sizeof(struct sockaddr_in6));
+  *p = uv_ip6_addr((const char*)StringValueCStr(ip), NUM2INT(port));
+  return Wrap(p, free);
+}
+
+//UV_EXTERN struct sockaddr_in uv_ip4_addr(const char* ip, int port);
+//UV_EXTERN struct sockaddr_in6 uv_ip6_addr(const char* ip, int port);
+
+/*
+UV_EXTERN int uv_tcp_init(uv_loop_t*, uv_tcp_t* handle);
+UV_EXTERN int uv_tcp_nodelay(uv_tcp_t* handle, int enable);
+UV_EXTERN int uv_tcp_keepalive(uv_tcp_t* handle, int enable,unsigned int delay);
+
+UV_EXTERN int uv_tcp_simultaneous_accepts(uv_tcp_t* handle, int enable);
+UV_EXTERN int uv_tcp_bind(uv_tcp_t* handle, struct sockaddr_in);
+UV_EXTERN int uv_tcp_bind6(uv_tcp_t* handle, struct sockaddr_in6);
+UV_EXTERN int uv_tcp_getsockname(uv_tcp_t* handle, struct sockaddr* name,
+    int* namelen);
+UV_EXTERN int uv_tcp_getpeername(uv_tcp_t* handle, struct sockaddr* name,
+    int* namelen);
+UV_EXTERN int uv_tcp_connect(uv_connect_t* req, uv_tcp_t* handle,
+    struct sockaddr_in address, uv_connect_cb cb);
+UV_EXTERN int uv_tcp_connect6(uv_connect_t* req, uv_tcp_t* handle,
+    struct sockaddr_in6 address, uv_connect_cb cb);
+*/
+static VALUE tcp_init(uv_loop_t* loop) {
+  InitHandle(tcp);
+}
+
+// UV_EXTERN int uv_tcp_nodelay(uv_tcp_t* handle, int enable)
+static VALUE tcp_nodelay(VALUE self, VALUE handle, VALUE enable) {
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  int enable_ = NUM2INT(enable);
+  int retval = uv_tcp_nodelay(handle_, enable_);
+  return Qnil;
+}
+
+// UV_EXTERN int uv_tcp_keepalive(uv_tcp_t* handle, int enable,unsigned int delay)
+static VALUE tcp_keepalive(VALUE self, VALUE handle, VALUE enable, VALUE delay) {
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  int enable_ = NUM2INT(enable);
+  unsigned int delay_ = NUM2UINT(delay);
+  int retval = uv_tcp_keepalive(handle_, enable_, delay_);
+  return INT2NUM(retval);
+}
+
+// UV_EXTERN int uv_tcp_simultaneous_accepts(uv_tcp_t* handle, int enable)
+static VALUE tcp_simultaneous_accepts(VALUE self, VALUE handle, VALUE enable) {
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  int enable_ = NUM2INT(enable);
+  int retval = uv_tcp_simultaneous_accepts(handle_, enable_);
+  return INT2NUM(retval);
+}
+
+
+// UV_EXTERN int uv_tcp_bind(uv_tcp_t* handle, struct sockaddr_in)
+static VALUE tcp_bind(VALUE self, VALUE handle, VALUE sockaddr_in) {
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  struct sockaddr_in* sockaddr_in_;
+  Data_Get_Struct(sockaddr_in, struct sockaddr_in, sockaddr_in_);
+  int retval = uv_tcp_bind(handle_, *sockaddr_in_);
+  return Qnil;
+}
+
+// UV_EXTERN int uv_tcp_bind6(uv_tcp_t* handle, struct sockaddr_in6)
+static VALUE tcp_bind6(VALUE self, VALUE handle, VALUE sockaddr_in6) {
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  struct sockaddr_in6* sockaddr_in6_;
+  Data_Get_Struct(sockaddr_in6, struct sockaddr_in6, sockaddr_in6_);
+
+  int retval = uv_tcp_bind6(handle_, *sockaddr_in6_);
+  return Qnil;
+}
+
+// UV_EXTERN int uv_tcp_getsockname(uv_tcp_t* handle, struct sockaddr* name,    int* namelen)
+static VALUE tcp_getsockname(VALUE self, VALUE handle) {
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  struct sockaddr* name_ = malloc(sizeof(struct sockaddr));
+  int size;
+  int retval = uv_tcp_getsockname(handle_, name_, &size);
+  return Wrap(name_, free);
+}
+
+// UV_EXTERN int uv_tcp_getpeername(uv_tcp_t* handle, struct sockaddr* name,    int* namelen)
+static VALUE tcp_getpeername(VALUE self, VALUE handle, VALUE name, VALUE namelen) {
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  struct sockaddr* name_ = malloc(sizeof(struct sockaddr));
+  int size;
+  int retval = uv_tcp_getpeername(handle_, name_, &size);
+  return Wrap(name_, free);
+}
+
+static void connect__callback(uv_connect_t* req, int status) {
+  VALUE argv[] = { Wrap(req,0), INT2NUM(status) };
+  callback_apply(req->handle->loop, req->data, 2, argv);
+}
+// UV_EXTERN int uv_tcp_connect(uv_connect_t* req, uv_tcp_t* handle,    struct sockaddr_in address, uv_connect_cb cb)
+static VALUE tcp_connect(VALUE self, VALUE req, VALUE handle, VALUE address, VALUE cb) {
+  uv_connect_t* req_;
+  Data_Get_Struct(req, uv_connect_t, req_);
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  struct sockaddr_in* address_;
+  Data_Get_Struct(address, struct sockaddr_in, address_);
+  req_->data = (void*)callback(cb);
+  int retval = uv_tcp_connect(req_, handle_, *address_, connect__callback);
+  return Qnil;
+}
+
+// UV_EXTERN int uv_tcp_connect6(uv_connect_t* req, uv_tcp_t* handle,    struct sockaddr_in6 address, uv_connect_cb cb)
+static VALUE tcp_connect6(VALUE self, VALUE req, VALUE handle, VALUE address, VALUE cb) {
+  uv_connect_t* req_;
+  Data_Get_Struct(req, uv_connect_t, req_);
+  uv_tcp_t* handle_;
+  Data_Get_Struct(handle, uv_tcp_t, handle_);
+  struct sockaddr_in6* address_;
+  Data_Get_Struct(address, struct sockaddr_in6, address_);
+  req_->data = (void*)callback(cb);
+  int retval = uv_tcp_connect6(req_, handle_, *address_, connect__callback);
+  return Qnil;
+}
+
 
 
 /*
