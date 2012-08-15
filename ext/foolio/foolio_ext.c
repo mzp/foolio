6fe011263bfea2a5caab2f1b54517ea204c725bc
mzp <mzpppp@gmail.com>
2012-08-15 23:39:13 +0900

    implement uv_timer_xxx and uv_idle_xxx

--- ext/foolio/foolio_ext.c
+++ ext/foolio/foolio_ext.c
@@ -6,38 +6,26 @@ void *  rb_thread_call_with_gvl(void *(*func)(void *), void *data1);
 
 #define Wrap(expr, f)   Data_Wrap_Struct(klass, 0, (f), (expr))
 #define Method(name, n) rb_define_singleton_method(klass, #name, name, n)
-#define Get(T, P, FROM) \
-  T* P; \
-  Data_Get_Struct(FROM, T, P);
+#define Decl(T) \
+  uv_##T##_t * T##_p; \
+  Data_Get_Struct(T, uv_##T##_t, T##_p)
+
+#define InitHandle(T)       \
+  uv_##T##_t * handle = malloc(sizeof(uv_##T##_t));  \
+  Decl(loop);      \
+  if( uv_##T##_init(loop_p, handle) == 0) {   \
+    return Wrap(handle, free);    \
+  } else {                        \
+    return Qnil;                  \
+  }                               \
+
+
+// Foolio::UV class
+VALUE klass;
 
 struct LoopData {
   bool gvl; // has GVL?
 };
-// ------------------------------
-// Loop
-// ------------------------------
-VALUE klass;
-static VALUE default_loop(VALUE self) {
-  return Wrap( uv_default_loop(), 0 );
-}
-
-static VALUE run_(void* loop) {
-  return INT2NUM(uv_run(loop));
-}
-
-static VALUE run(VALUE self, VALUE loop) {
-  Get(uv_loop_t, p, loop);
-  struct LoopData data = { false };
-  p->data = (void*)&data;
-  rb_thread_blocking_region(run_, (void*)p, RUBY_UBF_IO, NULL);
-}
-
-static VALUE run_once(VALUE self, VALUE loop) {
-  Get(uv_loop_t, p, loop);
-  struct LoopData data = { true };
-  p->data = (void*)&data;
-  return INT2NUM(uv_run_once(p));
-}
 
 // ------------------------------
 // callback
@@ -70,6 +58,8 @@ void callback_apply(uv_loop_t* loop, struct CallbackData* data, int argc, VALUE 
   if(p->gvl) {
     callback__apply((void*)data);
   } else {
+    // All callback function are run without gvl.
+    // But we need GVL to call Ruby methods.
     rb_thread_call_with_gvl(callback__apply, (void*)data);
   }
 }
@@ -78,18 +68,82 @@ void callback_free(struct CallbackData* data) {
   free(data);
 }
 
-// ------------
-// timer
-// ------------
-//UV_EXTERN int uv_timer_init(uv_loop_t*, uv_timer_t* timer);
+// ------------------------------
+// Loop
+// ------------------------------
+static VALUE default_loop(VALUE self) {
+  return Wrap( uv_default_loop(), 0 );
+}
+
+static VALUE loop_new(VALUE self){
+  return Wrap( uv_loop_new(), 0);
+}
+
+static VALUE loop_delete(VALUE self, VALUE loop) {
+  Decl(loop);
+  uv_loop_delete(loop_p);
+  return Qnil;
+}
+
+// ------------------------------
+// run
+// ------------------------------
+static VALUE run_(void* loop) {
+  return INT2NUM(uv_run(loop));
+}
+
+static VALUE run(VALUE self, VALUE loop) {
+  Decl(loop);
+  struct LoopData data = { false };
+  loop_p->data = (void*)&data;
+  rb_thread_blocking_region(run_, (void*)loop_p, RUBY_UBF_IO, NULL);
+}
+
+static VALUE run_once(VALUE self, VALUE loop) {
+  Decl(loop);
+  struct LoopData data = { true };
+  loop_p->data = (void*)&data;
+  return INT2NUM(uv_run_once(loop_p));
+}
+
+
+
+/*
+  UV_EXTERN int uv_idle_init(uv_loop_t*, uv_idle_t* idle);
+  UV_EXTERN int uv_idle_start(uv_idle_t* idle, uv_idle_cb cb);
+  UV_EXTERN int uv_idle_stop(uv_idle_t* idle);
+*/
+static VALUE idle_init(VALUE self, VALUE loop) {
+  InitHandle(idle);
+}
+
+static void idle_callback(uv_idle_t* handle, int status) {
+  VALUE argv[] = { Wrap(handle,0) , INT2NUM(status) };
+  callback_apply(handle->loop, handle->data, 2, argv);
+}
+
+static VALUE idle_start(VALUE self, VALUE idle, VALUE cb) {
+  Decl(idle);
+  idle_p->data = callback(cb);
+  uv_idle_start(idle_p, idle_callback);
+}
+
+static VALUE idle_stop(VALUE self, VALUE idle) {
+  Decl(idle);
+  callback_free(idle_p->data);
+  uv_idle_stop(idle_p);
+}
+
+/**
+   UV_EXTERN int uv_timer_init(uv_loop_t*, uv_timer_t* timer);
+   UV_EXTERN int uv_timer_start(uv_timer_t* timer, uv_timer_cb cb,
+   UV_EXTERN int uv_timer_stop(uv_timer_t* timer);
+   UV_EXTERN int uv_timer_again(uv_timer_t* timer);
+   UV_EXTERN void uv_timer_set_repeat(uv_timer_t* timer, int64_t repeat);
+   UV_EXTERN int64_t uv_timer_get_repeat(uv_timer_t* timer);
+ */
 static VALUE timer_init(VALUE self, VALUE loop) {
-  uv_timer_t* timer = malloc(sizeof(uv_timer_t));
-  Get(uv_loop_t, loop_data, loop);
-  if( uv_timer_init(loop_data, timer) == 0) {
-    return Wrap(timer, free);
-  } else {
-    return Qnil;
-  }
+  InitHandle(timer);
 }
 
 static void timer_callback(uv_timer_t* handle, int status) {
@@ -97,31 +151,38 @@ static void timer_callback(uv_timer_t* handle, int status) {
   callback_apply(handle->loop, handle->data, 2, argv);
 }
 
-//UV_EXTERN int uv_timer_start(uv_timer_t* timer, uv_timer_cb cb,
-//    int64_t timeout, int64_t repeat);
 static VALUE timer_start(VALUE self, VALUE timer, VALUE cb, VALUE timeout, VALUE repeat) {
-  Get(uv_timer_t, timer_t, timer);
-  int timeout_n = NUM2INT(timeout);
-  int repeat_n  = NUM2INT(repeat);
-  timer_t->data = (void*)callback(cb);
-  return INT2NUM(uv_timer_start(timer_t, timer_callback, timeout_n, repeat_n));
+  Decl(timer);
+  timer_p->data = (void*)callback(cb);
+  int retval =
+    uv_timer_start(timer_p, timer_callback, NUM2INT(timeout), NUM2INT(repeat));
+  return INT2NUM(retval);
 }
 
-// UV_EXTERN int uv_timer_stop(uv_timer_t* timer);
 static VALUE timer_stop(VALUE self, VALUE timer) {
-  Get(uv_timer_t, timer_data, timer);
-  return INT2FIX(uv_timer_stop(timer_data));
+  Decl(timer);
+  callback_free(timer_p->data);
+  return INT2FIX(uv_timer_stop(timer_p));
 }
 
-// UV_EXTERN int uv_timer_again(uv_timer_t* timer);
 static VALUE timer_again(VALUE self, VALUE timer) {
-  Get(uv_timer_t, timer_data, timer);
-  return INT2FIX(uv_timer_again(timer_data));
+  Decl(timer);
+  return INT2FIX(uv_timer_again(timer_p));
 }
-/*
-UV_EXTERN int uv_timer_start(uv_timer_t* timer, uv_timer_cb cb,
-    int64_t timeout, int64_t repeat);
-*/
+
+static VALUE timer_set_repeat(VALUE timer, VALUE repeat) {
+  Decl(timer);
+  int64_t repeat_n = NUM2LONG(repeat);
+  uv_timer_set_repeat(timer_p, repeat);
+  return Qnil;
+}
+
+static VALUE timer_get_repeat(VALUE timer) {
+  Decl(timer);
+  int64_t retval = uv_timer_get_repeat(timer_p);
+  return LONG2NUM(retval);
+}
+
 
 void Init_foolio_ext(void) {
   klass = rb_define_class_under(rb_define_module("Foolio"),
