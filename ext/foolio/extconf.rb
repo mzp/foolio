1e0b1ffed0d4d8fb2f00bdbafa430cf695e4b22d
mzp <mzpppp@gmail.com>
2012-08-23 22:38:49 +0900

    windows: add missing static library

--- ext/foolio/extconf.rb
+++ ext/foolio/extconf.rb
@@ -12,9 +12,23 @@ end
 libuv_dir = File.expand_path("../../libuv", __FILE__)
 foolio_dir = File.expand_path("../", __FILE__)
 
-system "cd '#{libuv_dir}'; CFLAGS='-fPIC' make; cd '#{foolio_dir}'; cp #{libuv_dir}/uv.a #{foolio_dir}/libuv.a"
+Dir.chdir(libuv_dir) do
+  system "make"
+end
+
+Dir.chdir(foolio_dir) do
+  require 'fileutils'
+  FileUtils.cp "#{libuv_dir}/uv.a", "#{foolio_dir}/libuv.a"
+end
 
 dir_config "uv", "#{libuv_dir}/include", foolio_dir
+
+case RbConfig::CONFIG['host_os'].downcase
+when /mingw|mswin/
+  have_library "ws2_32"
+  have_library "psapi"
+  have_library "iphlpapi"
+end
 have_library "uv"
 
 create_makefile "foolio/foolio_ext"
