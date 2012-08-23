92695f0c1b480f598951df97a02f9dfd47313902
unknown <mzp@mzp-vbox-air.(none)>
2012-08-23 10:31:25 +0900

    update

--- ext/foolio/extconf.rb
+++ ext/foolio/extconf.rb
@@ -12,9 +12,19 @@ end
 libuv_dir = File.expand_path("../../libuv", __FILE__)
 foolio_dir = File.expand_path("../", __FILE__)
 
-system "cd '#{libuv_dir}'; CFLAGS='-fPIC' make; cd '#{foolio_dir}'; cp #{libuv_dir}/uv.a #{foolio_dir}/libuv.a"
+Dir.chdir(libuv_dir) do 
+  system "make CFLAGS='-fPIC'"
+end
+
+Dir.chdir(foolio_dir) do
+  require 'fileutils'
+  FileUtils.cp "#{libuv_dir}/uv.a", "#{foolio_dir}/libuv.a"
+end
 
 dir_config "uv", "#{libuv_dir}/include", foolio_dir
+have_library "ws2_32"
+have_library "psapi"
+have_library "iphlpapi"
 have_library "uv"
 
 create_makefile "foolio/foolio_ext"
